f51d6204f041948371728644066f4c2a62da3a08
jens <jensmonne@tuta.com>
2026-05-18 12:14:21 +0200

    steamworks

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,18 +1,78 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // [Header("UI Elements")]
-    // [SerializeField] private Button startButton;
-    // [SerializeField] private Button settingsButton;
-    // [SerializeField] private Button creditsButton;
-    // [SerializeField] private Button exitButton;
+    [Header("UI Elements")]
+    [SerializeField] private Button hostButton;
+    [SerializeField] private TMP_InputField joinCodeInput;
+    [SerializeField] private Button joinButton;
+    [SerializeField] private TMP_Text statusText;
 
-    public void OnStartPressed()
+    private void Start()
     {
-        MenuManager.Instance.OpenMenu("StartMenu");
+        if (UnityAuthInitializer.IsAuthenticated) EnableOnlineButton();
     }
-    
+
+    private void OnEnable()
+    {
+        UnityAuthInitializer.OnAuthenticated += EnableOnlineButton;
+    }
+
+    private void OnDestroy()
+    {
+        UnityAuthInitializer.OnAuthenticated -= EnableOnlineButton;
+    }
+
+    private void EnableOnlineButton()
+    {
+        hostButton.interactable = true;
+        joinCodeInput.interactable = true;
+        statusText.text = "Play locally or online by hosting or joining a game.";
+    }
+
+    public void OnLocalPressed()
+    {
+        statusText.text = "Starting local game...";
+
+        CustomNetworkManager.singleton.StartLocalGame();
+    }
+
+    public void OnHostPressed()
+    {
+        statusText.text = "Starting online game...";
+        
+        int maxPlayers = 4; // Later maybe make this a user input
+
+        CustomNetworkManager.singleton.StartRelayHost(maxPlayers);
+    }
+
+    public void OnCodeInputChanged()
+    {
+        string codeInputText = joinCodeInput.text.Trim();
+        joinCodeInput.text = codeInputText.ToUpper();
+        joinButton.interactable = !string.IsNullOrEmpty(codeInputText);
+    }
+
+    public void OnJoinPressed()
+    {
+        string code = joinCodeInput.text.Trim();
+
+        if (string.IsNullOrEmpty(code))
+        {
+            statusText.text = "Please enter a join code.";
+            return;
+        }
+
+        CustomNetworkManager.singleton.JoinRelayGame(code);
+    }
+
+    public void OnBackPressed()
+    {
+        MenuManager.Instance.OpenMenu("MainMenu");
+    }
+
     public void OnSettingsPressed()
     {
         MenuManager.Instance.OpenMenu("SettingsMenu");
---
+++ Assets/Testing/scripts/SteamInit.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SteamInit : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
