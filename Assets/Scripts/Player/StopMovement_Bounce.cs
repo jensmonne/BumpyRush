78887fcc6af9763300b9f8113225b3fab22b2150
Marinus <marijnvanveggel3@gmail.com>
2026-05-19 12:35:49 +0200

    Bumpy doe bumpy

--- Assets/Scripts/Player/StopMovement_Bounce.cs
+++ Assets/Scripts/Player/StopMovement_Bounce.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 
 public class StopMovement_Bounce : MonoBehaviour
 {
-    [SerializeField] private Collider targetCollider;
-    [SerializeField] private float stoppingMovementDuration = 1f;
+    [SerializeField] private Collider BounceCollider;
+    [SerializeField] private Rigidbody CartRigidbody;
+
+    [Header("Collision Duration")]
+    [SerializeField] private float minimumDuration = 0.2f;
+    [SerializeField] private float maximumDuration = 3f;
+    [SerializeField] private float impactMultiplier = 0.2f;
+
+    [Header("Bounce Force")]
+    [SerializeField] private float bounceForceMultiplier = 10f;
+    [SerializeField] private float maxBounceForce = 30f;
 
     public bool hasCollided = false;
 
@@ -12,24 +21,47 @@ public class StopMovement_Bounce : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.GetContact(0).thisCollider != targetCollider)
-            return;
-
-        if (stopCoroutine != null)
+        foreach (ContactPoint contact in collision.contacts)
         {
-            StopCoroutine(stopCoroutine);
+            if (contact.thisCollider == BounceCollider)
+            {
+                float impactForce = collision.relativeVelocity.magnitude;
+
+                // Duration
+                float duration = impactForce * impactMultiplier;
+                duration = Mathf.Clamp(duration, minimumDuration, maximumDuration);
+
+                // Bounce
+                Vector3 bounceDirection = contact.normal;
+
+                float bounceForce = impactForce * bounceForceMultiplier;
+                bounceForce = Mathf.Clamp(bounceForce, 0f, maxBounceForce);
+
+                CartRigidbody.AddForce(
+                    bounceDirection * bounceForce,
+                    ForceMode.Impulse
+                );
+
+                if (stopCoroutine != null)
+                {
+                    StopCoroutine(stopCoroutine);
+                }
+
+                stopCoroutine = StartCoroutine(StopMovementTimer(duration));
+
+                Debug.Log("Impact: " + impactForce);
+                Debug.Log("Bounce Force: " + bounceForce);
+
+                return;
+            }
         }
-
-        stopCoroutine = StartCoroutine(StopMovementTimer());
-
-        Debug.Log("Correct collider hit!");
     }
 
-    private IEnumerator StopMovementTimer()
+    private IEnumerator StopMovementTimer(float duration)
     {
         hasCollided = true;
 
-        yield return new WaitForSeconds(stoppingMovementDuration);
+        yield return new WaitForSeconds(duration);
 
         hasCollided = false;
     }
