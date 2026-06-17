46e35e32b4be8207861cc7717ac3d3d4d38c4834
jens <jensmonne@tuta.com>
2026-06-17 16:24:29 +0200

    Namedisplay

--- Assets/Scripts/UI/Components/NameDisplayLock.cs
+++ Assets/Scripts/UI/Components/NameDisplayLock.cs
@@ -4,28 +4,34 @@ public class NameDisplayLock : MonoBehaviour
 {
     private Transform localCameraTransform;
 
-    void Start()
+    private void Start()
+    {
+        CacheLocalCamera();
+    }
+
+    private void LateUpdate()
+    {
+        if (localCameraTransform == null)
+        {
+            CacheLocalCamera();
+            if (localCameraTransform == null) {
+                Debug.LogWarning("No main camera found. NameDisplayLock will not function properly.");
+                return;
+            };
+        }
+
+        transform.LookAt
+        (
+            transform.position + localCameraTransform.rotation * Vector3.forward,
+            localCameraTransform.rotation * Vector3.up
+        );
+    }
+
+    private void CacheLocalCamera()
     {
         if (Camera.main != null)
         {
             localCameraTransform = Camera.main.transform;
         }
-    }
-
-    void LateUpdate()
-    {
-        if (localCameraTransform == null)
-        {
-            if (Camera.main != null)
-            {
-                localCameraTransform = Camera.main.transform;
-            }
-            return;
-        }
-
-        transform.LookAt(
-            transform.position + localCameraTransform.rotation * Vector3.forward,
-            localCameraTransform.rotation * Vector3.up
-        );
     }
 }
