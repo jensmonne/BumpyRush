8edbc018ff3a3149b4b43996e8b2fd1ef748cb70
S0ra-0 <79579919+S0ra-0-0@users.noreply.github.com>
2026-06-30 01:17:13 +0200

    Oil Spill knockback fix

--- Assets/Scripts/Gameplay/PowerUps/Oil spill/OilSpill.cs
+++ Assets/Scripts/Gameplay/PowerUps/Oil spill/OilSpill.cs
@@ -8,7 +8,9 @@ public class OilSpill : MonoBehaviour
         {
             Debug.Log("Player hit the oil spill!" + collision.gameObject.name);
             Rigidbody rb = collision.gameObject.GetComponentInParent<Rigidbody>();
-            rb.AddExplosionForce(1500, transform.position, 5f, 20f, ForceMode.Impulse);
+            Vector3 com = rb.worldCenterOfMass;
+            Vector3 explosionPos = new Vector3(com.x + 0.1f, transform.position.y, com.z);
+            rb.AddExplosionForce(5000, explosionPos, 5f, 20f, ForceMode.Impulse);
             Destroy(gameObject);
         }
     }
